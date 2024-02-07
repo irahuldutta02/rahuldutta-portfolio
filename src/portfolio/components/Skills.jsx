1ee05fb15fb126bfc70a82d33c45e20da22c3f03
irahuldutta02 <78687135+irahuldutta02@users.noreply.github.com>
2024-02-07 16:35:44 +0530

    implemented skills data from data file logic

--- src/portfolio/components/Skills.jsx
+++ src/portfolio/components/Skills.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import "../portfolio.css";
+import { getTechStack } from "../../data/data";
 
 export function Skills() {
+  const [techStack] = useState(getTechStack());
+
   return (
     <>
       {/* <!-- skills section start --> */}
@@ -9,133 +13,16 @@ export function Skills() {
           <h2 className="title">Tech stack</h2>
           <div className="skills-content">
             <div className="inside-coloum">
-              <div>
-                <span>HTML</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/fluency/48/html-5.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>CSS</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/fluency/48/css3.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                {" "}
-                <span>Bootstrap</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/fluency/48/bootstrap.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Tailwind</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/tailwindcss.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>JavaScript</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/javascript.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>EJS</span>
-                <span className="icon">
-                  <img src="https://img.icons8.com/color/48/ejs.png" alt="" />
-                </span>
-              </div>
-              <div>
-                <span>Express Js</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/fluency/48/express-js.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Node Js</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/nodejs.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Java</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/java-coffee-cup-logo.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Java JDBC</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/database.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>MySql</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/mysql-logo.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>MongoDB</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/color/48/mongodb.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Git</span>
-                <span className="icon">
-                  <img src="https://img.icons8.com/color/48/git.png" alt="" />
-                </span>
-              </div>
-              <div>
-                <span>GitHub</span>
-                <span className="icon">
-                  <img
-                    src="https://img.icons8.com/fluency/48/github.png"
-                    alt=""
-                  />
-                </span>
-              </div>
-              <div>
-                <span>Figma</span>
-                <span className="icon">
-                  <img src="https://img.icons8.com/color/48/figma.png" alt="" />
-                </span>
-              </div>
+              {techStack.map((tech) => {
+                return (
+                  <div key={tech.id}>
+                    <span>{tech.name}</span>
+                    <span className="icon">
+                      <img src={tech.iconSrc} alt={tech.name} />
+                    </span>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
